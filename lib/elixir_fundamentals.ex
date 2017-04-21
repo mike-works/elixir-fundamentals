9fd68c2f156e6e7e2df03ed1b5f481d9774d8dc8
Mike North <michael.l.north@gmail.com>
2017-04-21 09:26:00 -0700

    initial commit

---
+++ lib/elixir_fundamentals.ex
@@ -0,0 +1,18 @@
+defmodule ElixirFundamentals do
+  @moduledoc """
+  Documentation for ElixirFundamentals.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirFundamentals.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_fundamentals_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirFundamentalsTest do
+  use ExUnit.Case
+  doctest ElixirFundamentals
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
