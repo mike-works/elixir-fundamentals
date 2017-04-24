51b423b9510b80e677400b79872416511a2c1f2e
Mike North <michael.l.north@gmail.com>
2017-04-24 14:28:02 +0200

    cleanup - exercise 4

--- lib/github.ex
+++ lib/github.ex
@@ -1,16 +1,35 @@
 defmodule Github do
-  def org_url(org_name) when is_bitstring(org_name), do: "https://api.github.com/orgs/#{org_name}?access_token=22ba634a0d3280f10c9245b6eb49b5800f6b3b29"
-  def repo_url(owner_name, repo_name) when is_bitstring(owner_name) and is_bitstring(repo_name), do: "https://api.github.com/repos/#{owner_name}/#{repo_name}?access_token=22ba634a0d3280f10c9245b6eb49b5800f6b3b29"
 
-  def org(org_name), do: json_data(org_url(org_name))
-  def repo(owner_name, repo_name), do: json_data(repo_url(owner_name, repo_name))
+  @api_key  Application.get_env(:elixir_fundamentals, :github_token)
+  @api_base Application.get_env(:elixir_fundamentals, :github_api)
 
-  defp json_data(url) do
-    %{body: body_string, status_code: status} = HTTPoison.get!(url);
-    body_json = Poison.decode!(body_string)
-    handle_json_response(status, body_json)
+  def org_url(org_name) when is_bitstring(org_name), do: "#{@api_base}/orgs/#{org_name}"
+  def repo_url(owner_name, repo_name) when is_bitstring(owner_name) and is_bitstring(repo_name), do: "#{@api_base}/repos/#{owner_name}/#{repo_name}"
+
+  def org(org_name) do
+    org_name
+    |> org_url
+    |> json_data
   end
 
-  defp handle_json_response(200, data), do: {:ok, data}
-  defp handle_json_response(status, data), do: {:error, data}
+  def repo(owner_name, repo_name) do
+    owner_name
+    |> repo_url(repo_name) # Build the URL
+    |> json_data # Return the data
+  end
+
+  defp json_data(url) do
+    import HTTPoison, only: [get!: 3]
+    import Poison, only: [decode!: 1]
+
+    %{body: body_string, status_code: status} = url
+    |> get!([], params: %{access_token: @api_key})
+
+    body_string
+    |> decode!
+    |> handle_json_response(status)
+  end
+
+  defp handle_json_response(data, 200), do: {:ok, data}
+  defp handle_json_response(data, status), do: {:error, data}
 end
