6f63caf1a74e30b16e9457c0272d9b8934b34d9a
Mike North <michael.l.north@gmail.com>
2017-04-24 14:52:27 +0200

    exercise 5 - credit card

--- lib/credit_card.ex
+++ lib/credit_card.ex
@@ -1,3 +1,8 @@
 defmodule CreditCard do
-  # defstruct 
+  defstruct type: "", expire_year: nil
+  @valid_types ["visa", "mastercard", "amex"]
+
+  def process(%CreditCard{type: type, expire_year: yr}) when (type in @valid_types) and (yr >= 2017) do
+    "OK!"
+  end
 end
