911168b5fac6124274f60a13fce031bad056531a
Mike North <michael.l.north@gmail.com>
2017-04-25 08:57:03 +0200

    tic tac toe

---
+++ lib/tic_tac_toe.ex
@@ -0,0 +1,11 @@
+defmodule TicTacToe do
+  def new do
+    
+  end
+  def make_move(game, player, {x, y}) do
+   
+  end
+  def get_board(game) do
+ 
+  end
+end
---
+++ test/tic_tac_toe.exs
@@ -0,0 +1,29 @@
+defmodule TicTacToeTest do
+  use ExUnit.Case
+  @moduletag :exercise14
+
+  test "Starting a game should return a PID" do
+    game = TicTacToe.new
+    assert is_pid(game) === true
+  end
+
+  test "Board is full of nils before game starts" do
+    game = TicTacToe.new
+
+    board_before = game |> TicTacToe.get_board
+    assert board_before[1][1] === nil
+  end
+
+  test "Making a move should change game state" do
+    game = TicTacToe.new
+
+    board_before = game |> TicTacToe.get_board
+    game
+    |> TicTacToe.make_move("X", {1, 1})
+
+    board_after = game |> TicTacToe.get_board
+    assert board_after[1][1] === "x"
+  end
+
+
+end
