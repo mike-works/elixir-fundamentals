5c11316872dc75107ce81b3cf928c16239c2bd7b
Mike North <michael.l.north@gmail.com>
2017-04-25 10:57:59 +0200

    exercise9

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,11 +1,16 @@
 defmodule FizzBuzz do
   def play_game(number) when number >= 1 do
-    1..number
-    |> Enum.map(&player_turn/1)
+    1..number # [1, 2, 3, 4, 5, 6]
+    |> Enum.map(&player_turn/1) # [1, 2, "Fizz", 4, "Buzz", "FizzBuzz"]
     |> Enum.join(", ")
   end
 
   defp player_turn(number) do
-    number # Replace with your real answer
+    case number do
+      _ when rem(number, 15) === 0 -> "FizzBuzz"
+      _ when rem(number, 3) === 0 -> "Fizz"
+      _ when rem(number, 5) === 0 -> "Buzz"
+      x -> x 
+    end
   end
 end
