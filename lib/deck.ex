149ebc66f2055ea98d6b8f0cab739129b8dffb0b
Mike North <michael.l.north@gmail.com>
2017-05-13 12:11:24 +0200

    comprehensions

--- lib/deck.ex
+++ lib/deck.ex
@@ -1,5 +1,10 @@
 defmodule Deck do
+  @deck for suit <- ~w(H C S D),
+          card <- ~w(A 2 3 4 5 6 7 8 9 10 J Q K),
+          do: "#{card}#{suit}"
   def get_cards(num) when num > 0 do
-    [] # TODO: deal some cards
+    @deck
+    |> Enum.shuffle
+    |> Enum.take(num)
   end
 end
