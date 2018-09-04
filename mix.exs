f5958de8d7e097609ad6090f45e7be991f574445
Mike North <michael.l.north@gmail.com>
2018-09-03 22:41:48 -0700

    fix: upgrade elixir to 1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirFundamentals.Mixfile do
   def project do
     [app: :elixir_fundamentals,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
